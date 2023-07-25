fbfafea20a4d07baa3bb358ed664c41568b6e3f8
kinggq <18701094030@163.com>
2023-07-25 18:42:08 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,195 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+<script setup lang="ts">
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines: number
+}
 
-const name = ref('')
+const WIDTH = 5
+const HEIGHT = 5
 
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+const state = reactive(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from(
+      { length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+      }),
+    ),
+  ),
+)
+
+function expendZero(block: BlockState) {
+  if (block.adjacentMines)
+    return
+
+  getSiblings(block).forEach((s) => {
+    if (!s.revealed) {
+      s.revealed = true
+      expendZero(s)
+    }
+  })
+}
+
+let mineGenerated = false
+const dev = false
+
+function onClick(block: BlockState) {
+  if (block.flagged)
+    return
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
+  block.revealed = true
+  if (block.mine)
+    alert('BOOOM!')
+
+  expendZero(block)
+}
+
+function onRightClick(block: BlockState) {
+  if (block.revealed)
+    return
+  block.flagged = !block.flagged
+}
+
+function generateMines(initial: BlockState) {
+  for (const row of state) {
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1)
+        continue
+      if (Math.abs(initial.y - block.y) <= 1)
+        continue
+      block.mine = Math.random() < 0.2
+    }
+  }
+  updateNumbers()
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+
+function updateNumbers() {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+      getSiblings(block)
+        .forEach((b) => {
+          if (b.mine)
+            block.adjacentMines += 1
+        })
+    })
+  })
+}
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+  'text-teal-500',
+]
+
+function getSiblings(block: BlockState) {
+  return directions.map(([dx, dy]) => {
+    const x2 = block.x + dx
+    const y2 = block.y + dy
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return undefined
+    return state[y2][x2]
+  })
+    .filter(Boolean) as BlockState[]
+}
+
+function getBlockClass(block: BlockState) {
+  if (block.flagged)
+    return 'bg-gray-500/10'
+
+  if (!block.revealed)
+    return 'bg-gray-500/10 hover:bg-gray-500/20'
+  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
+}
+
+watchEffect(checkGameState)
+function checkGameState() {
+  if (!mineGenerated)
+    return
+  const blocks = state.flat()
+  if (blocks.every(block => block.revealed || block.flagged)) {
+    if (blocks.some(block => block.flagged && !block.mine))
+      alert('You cheat!')
+    else
+      alert('You win!')
+  }
 }
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
+    <div>Minesweeper</div>
+    <div
+      v-for="(row, y) in state"
+      :key="y"
+      flex="~"
+      items-center
+      justify-center
+    >
       <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+        v-for="(block, x) in row"
+        :key="x"
+        border="1 gray-400/10"
+        flex="~"
+        m="0.5"
+        h-10
+        w-10
+        items-center
+        justify-center
+        :class="getBlockClass(block)"
+        @click="onClick(block)"
+        @contextmenu.prevent="onRightClick(block)"
       >
-        Go
+        <template v-if="block.flagged">
+          <div
+            i-mdi:flag
+            text-red
+          />
+        </template>
+        <template v-else-if="block.revealed || dev">
+          <div
+            v-if="block.mine"
+            i-mdi:mine
+          >
+            x
+          </div>
+          <div v-else>
+            {{ block.adjacentMines }}
+          </div>
+        </template>
       </button>
     </div>
+    <div
+      class="a"
+      h-10
+      w-1
+    />
   </div>
 </template>
